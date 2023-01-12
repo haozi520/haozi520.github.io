6bddfba82746e99323bc0f1c18b5e85d69fa374b
haozi520 <2519890694@qq.com>
2023-01-12 12:35:52 +0800

    Site updated: 2023-01-12 12:35:52

--- js/duration.js
+++ js/duration.js
@@ -1,6 +1,6 @@
 !(function() {
   /** 计时起始时间，自行修改 **/
-  var start = new Date("2020/01/01 00:00:00");
+  var start = new Date("2022/11/17 11:45:14");
 
   function update() {
     var now = new Date();
