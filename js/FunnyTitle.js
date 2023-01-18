4735d3d5f4d0435fa9d51e4af391ed6167672efe
haozi520 <2519890694@qq.com>
2023-01-18 10:47:23 +0800

    Site updated: 2023-01-18 10:47:23

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -4,12 +4,12 @@
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
          $('[rel="icon"]').attr('href', "/img/trhx2.png");
-         document.title = 'ヽ(●-`Д´-)ノ你丑你就走！';
+         document.title = 'ヽ(●-`Д´-)ノ记得回来哦！'+ OriginTitle;
          clearTimeout(titleTime);
      }
      else {
          $('[rel="icon"]').attr('href', "/img/trhx2.png");
-         document.title = 'ヾ(Ő∀Ő3)ノ你帅就回来！' + OriginTitle;
+         document.title = 'ヾ(Ő∀Ő3)ノ欢迎回来！' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
